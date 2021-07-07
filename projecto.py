3d93226a35ad4927909c81fed6bf538333ad77a8
UnderDash3 <86974881+UnderDash3@users.noreply.github.com>
2021-07-06 21:58:10 -0400

    Create projecto.py

---
+++ projecto.py
@@ -0,0 +1,39 @@
+menup=int(input("ver graficas: \n 1.-contagiados sin sintomas acumulativos \n 2.-contagiados con sintomas acumulativos \n"))
+while menup != 0:
+    if menup == 1:
+        print("Arica y Parinacota")
+    elif  menup  == 2:
+        print("Tarapaca")
+    elif menup == 3:
+        print("Antofagasta")
+    elif menup == 4:
+        print("Atacama")
+    elif menup == 5:
+        print("Coquimbo")
+    elif menup == 6:
+        print("Valparaiso")
+    elif menup == 7:
+        print("Region Metropolitana De Santiago")
+    elif menup == 8:
+        print("Region Del Libertador General Bernardo O'Higgins")
+    elif menup == 9:
+        print("Maule")
+    elif menup == 10:
+        print("Ñuble")
+    elif menup == 11:
+        print("Biobio")
+    elif menup == 12:
+        print("La Araucania")
+    elif menup == 13:
+        print("Los Rios")
+    elif menup == 14:
+        print("Los Lagos")
+    elif menup == 15:
+        print("Aysén")
+    elif menup == 16:
+        print("Magallanes y La Antartica Chilena")
+
+        
+    else: print("porfavor digita una opcion")
+    
+    menup=int(input("ver graficas: \n 1.-contagiados sin sintomas acumulativos \n 2.-contagiados con sintomas acumulativos \n"))
